2ae84db55b3e363fb7d9557a1bba0ab5faae38ed
Souravroy99 <souravroy200418@gmail.com>
2026-01-12 00:51:38 +0530

    Created the create post controller

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,6 +1,21 @@
+import POST from "../models/post.model.js"
+
 export const getAllPosts = async (req, res) => {
     try {
-        return res.send("WORKS")
+        const posts = await POST.find()
+        return res.status(200).json(posts)
+    } catch (error) {
+        return res.status(500).json({ message: "Server error!" })
+    }
+}
+
+export const createPost = async (req, res) => {
+    try {
+        const post = req.body
+        const newPost = new POST(post)
+        await POST.save()
+
+        return res.status(201).json(newPost)
     } catch (error) {
         return res.status(500).json({ message: "Server error!" })
     }
